f565a738c633954ede575aeb0b30cec57cf4aafd
vortexishereforhelp-creator <vortexishereforhelp@gmail.com>
2026-05-09 16:04:07 +0100

    Update opts.rs

--- src/opts.rs
+++ src/opts.rs
@@ -1,6 +1,6 @@
-pub const BACKEND_URL: &str = "http://192.168.1.69:8080"; // Set valid IP, not 127.0.0.1 for mobile
+pub const BACKEND_URL: &str = "209.25.141.23:3349"; // Set valid IP, not 127.0.0.1 for mobile
 
 // Partyhub shit
 pub const USE_PARTYHUB: bool = false;
-pub const LOGIN_URL: &str = "http://192.168.1.69:5000/login";
-pub const REGISTER_URL: &str = "http://192.168.1.69:5000/register";
+pub const LOGIN_URL: &str = "http://209.25.141.23:1070/login";
+pub const REGISTER_URL: &str = "209.25.141.23:1070/register";
